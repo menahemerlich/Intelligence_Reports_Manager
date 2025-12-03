bb5209cc36189734427b42f8bd8fb2830b697cf3
menahem_erlich <erlichmenahem@gmail.com>
2025-12-03 17:16:07 +0200

    finish

--- utils/input_loop.js
+++ utils/input_loop.js
@@ -4,6 +4,10 @@ function inputLoop(num){
     let updates = {}
     for (let index = 0; index < num; index++) {
         let key_up = input('key: ');
+        if (key_up === 'id') {
+            throw new Error("ID cannot be changed.");
+            
+        }
         let val_up = input('val: ')
         updates[key_up] = val_up
     }
